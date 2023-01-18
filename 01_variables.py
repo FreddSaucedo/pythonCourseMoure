75a4317146d998b9c4eb70e931eb5ff557ab2f1a
FreddSaucedo <alfredosaucedorangel@gmail.com>
2023-01-17 19:10:13 -0600

    str() - Cambio de tipo de variables

---
+++ 01_variables.py
@@ -0,0 +1,11 @@
+# Variables
+myVariable = 'My String Variable'
+
+int_varibale = 1
+
+intToString = 323232323
+print(intToString)
+
+myIntoIntoString = str(intToString)
+print(myIntoIntoString)
+print(type(myIntoIntoString))
